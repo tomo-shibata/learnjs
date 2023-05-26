237a12214b22d466a35708c7b8de5b706c18a8f1
tomoya.shibata <tomo.shibata@gmail.com>
2023-05-26 14:38:02 +0900

    End 3.2.1

--- public/app.js
+++ public/app.js
@@ -1,18 +1,32 @@
 'use strict';
 
-var learnjs = {};
+const learnjs = {};
 
-learnjs.problemView = function (problemNumber) {
-    var title = 'Problem #' + problemNumber + ' Coming soon!';
-    return $('<div class="problem-view">').text(title);
+learnjs.problems = [
+    {
+        description: "What is truth?",
+        code: "function problem() { return __; }"
+    },
+    {
+        description: "Simple Math",
+        code: "function problem() { return 42 === 6 * __; }"
+    }
+]
+
+learnjs.problemView = function (data) {
+    const problemNumber = parseInt(data, 10);
+    const view = $('.templates .problem-view').clone();
+    view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(learnjs.problems[problemNumber - 1],view);
+    return view;
 }
 
 learnjs.showView = function (hash) {
-    var routes = {
+    const routes = {
         '#problem': learnjs.problemView
     };
-    var hashParts = hash.split('-');
-    var viewFn = routes[hashParts[0]];
+    const hashParts = hash.split('-');
+    const viewFn = routes[hashParts[0]];
     if (viewFn) {
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
@@ -23,4 +37,10 @@ learnjs.appOnReady = function () {
         learnjs.showView(window.location.hash);
     };
     learnjs.showView(window.location.hash);
+}
+
+learnjs.applyObject = function (obj, elm) {
+    for (const key in obj) {
+        elm.find('[data-name="' + key + '"]').text(obj[key]);
+    }
 }
