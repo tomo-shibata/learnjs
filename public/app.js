cd8e548fb779c0569e432187ecb73ae78f7c1402
tomoya.shibata <tomo.shibata@gmail.com>
2023-05-29 11:27:09 +0900

    End secion.3

--- public/app.js
+++ public/app.js
@@ -2,19 +2,35 @@
 
 const learnjs = {};
 
+learnjs.problems = [
+    {
+        description: "What is truth?",
+        code: "function problem() { return __; }"
+    },
+    {
+        description: "Simple Math",
+        code: "function problem() { return 42 === 6 * __; }"
+    }
+]
+
+learnjs.triggerEvent = function(name, args) {
+    $('.view-container>*').trigger(name, args);
+}
+learnjs.template = function(name) {
+    return $('.templates .' + name).clone();
+}
+learnjs.applyObject = function (obj, elm) {
+    for (const key in obj) {
+        elm.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+}
+
 learnjs.flashElement = function (elem, content) {
     elem.fadeOut('fast', function () {
         elem.html(content);
         elem.fadeIn();
     });
-}
 
-learnjs.template = function(name) {
-    return $('.templates .' + name).clone();
-}
-
-learnjs.landingView = function () {
-    return learnjs.template('landing-view');
 }
 
 learnjs.buildCorrectFlash = function (problemNum) {
@@ -27,44 +43,47 @@ learnjs.buildCorrectFlash = function (problemNum) {
         link.text("You're Finished!");
     }
     return correctFlash;
-}
 
-learnjs.problems = [
-    {
-        description: "What is truth?",
-        code: "function problem() { return __; }"
-    },
-    {
-        description: "Simple Math",
-        code: "function problem() { return 42 === 6 * __; }"
-    }
-]
+}
 
 learnjs.problemView = function (data) {
     const problemNumber = parseInt(data, 10);
     const view = learnjs.template('problem-view');
     const problemData = learnjs.problems[problemNumber - 1];
     const resultFlash = view.find('.result');
-    function checkAnswer() {
 
+    function checkAnswer() {
         const answer = view.find('.answer').val();
         const test = problemData.code.replace('__', answer) + '; problem();';
         return eval(test);
     }
     function checkAnswerClick() {
         if (checkAnswer()) {
-            learnjs.flashElement(resultFlash,
-                learnjs.buildCorrectFlash(problemNumber));
+            const flashContent = learnjs.buildCorrectFlash(problemNumber);
+            learnjs.flashElement(resultFlash, flashContent);
         } else {
             learnjs.flashElement(resultFlash, 'Incorrect!');
         }
         return false;
     }
 
+    if( problemNumber < learnjs.problems.length) {
+        const buttonItem = learnjs.template('skip-btn');
+        buttonItem.find('a').attr('href', '#problem-' +(problemNumber + 1));
+        $('.nav-list').append(buttonItem);
+        view.bind('removingView', function () {
+           buttonItem.remove();
+        });
+    }
+
     view.find('.check-btn').click(checkAnswerClick);
     view.find('.title').text('Problem #' + problemNumber);
     learnjs.applyObject(problemData, view);
     return view;
+}
+
+learnjs.landingView = function () {
+    return learnjs.template('landing-view');
 }
 
 learnjs.showView = function (hash) {
@@ -76,6 +95,7 @@ learnjs.showView = function (hash) {
     const hashParts = hash.split('-');
     const viewFn = routes[hashParts[0]];
     if (viewFn) {
+        learnjs.triggerEvent('removingView', []);
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 }
@@ -85,10 +105,4 @@ learnjs.appOnReady = function () {
         learnjs.showView(window.location.hash);
     };
     learnjs.showView(window.location.hash);
-}
-
-learnjs.applyObject = function (obj, elm) {
-    for (const key in obj) {
-        elm.find('[data-name="' + key + '"]').text(obj[key]);
-    }
 }
